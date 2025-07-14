d924f2df8c230d32b78fc65d7e9b400cd830d956
duaaayush <duaaayush29@gmail.com>
2025-07-14 16:57:19 +0530

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all IPs, not just localhost
+    port: 5173,       // Or any port you prefer
+  }
 })
